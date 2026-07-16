efd59d774d4b14a2c359a14427223a860848837c
Daniela Márquez <danielamarqu17@gmail.com>
2026-07-16 17:34:10 -0400

    eliminacíón de importaciones comentadas main

--- app/main.py
+++ app/main.py
@@ -9,10 +9,8 @@ from app.middlewares.logging_middleware import LoggingMiddleware
 from app.bd.database import async_engine, Base
 
 from app.routers import(
-    #categoria_maquina_router,
     cliente_router,
     control_acceso_router,
-    #disciplina_router,
     entrenador_router,
     evaluacion_biometrica_router,
     maquina_router,
@@ -20,11 +18,9 @@ from app.routers import(
     plan_suscripcion_router,
     producto_tienda_router,
     reserva_inscripcion_router,
-    #rol_router,
     sesion_programada_router,
     ticket_mantenimiento_router,
     usuario_router,
-    #venta_detalle_router,
     venta_tienda_router,
     pago_factura_router
 )
@@ -60,10 +56,9 @@ app.add_middleware(LoggingMiddleware)
 
 setup_exception_handlers(app)
 
-#app.include_router(categoria_maquina_router)
+
 app.include_router(cliente_router.router)
 app.include_router(control_acceso_router.router)
-#app.include_router(disciplina_router)
 app.include_router(entrenador_router.router)
 app.include_router(evaluacion_biometrica_router.router)
 app.include_router(maquina_router.router)
@@ -72,9 +67,7 @@ app.include_router(pago_factura_router.router)
 app.include_router(plan_suscripcion_router.router)
 app.include_router(producto_tienda_router.router)
 app.include_router(reserva_inscripcion_router.router)
-#app.include_router(rol_router)
 app.include_router(sesion_programada_router.router)
 app.include_router(ticket_mantenimiento_router.router)
 app.include_router(usuario_router.router)
-#app.include_router(venta_detalle_router)
 app.include_router(venta_tienda_router.router)
